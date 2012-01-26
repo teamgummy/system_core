8b7ba94891ff0fe8e2e4d22471f94293ab0a8638
Hashcode <hashcode0f@gmail.com>
2012-01-26 02:13:43 -0800

    added BURST_IMAGE flag for camera
    Change-Id: I10cd83dbffbfe90e606323f956be928135fcb368

--- include/system/camera.h
+++ include/system/camera.h
@@ -85,6 +85,7 @@ enum {
     // request FRAME and METADATA. Or the apps can request only FRAME or only
     // METADATA.
     CAMERA_MSG_PREVIEW_METADATA = 0x0400, // dataCallback
+    CAMERA_MSG_BURST_IMAGE = 0x0800, // dataCallback
     CAMERA_MSG_ALL_MSGS = 0xFFFF
 };
 
